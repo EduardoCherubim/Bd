bad6ed4af8b912183b9b2e564da80054c73eba36
EduardoCherubim <142914224+EduardoCherubim@users.noreply.github.com>
2024-03-10 16:40:09 -0300

    Atualizar o SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -2,7 +2,7 @@ USE bdEstoque
 
 INSERT INTO tbCliente (nomeCliente,cpfCliente,emailCliente,sexoCliente,dataNascimento)	
 VALUES
-	('Amando JosÈ Santana','123.456.789-00','amandojsanatana@outlook.com','M','21/02/1961')
+	('Amando Jos√© Santana','123.456.789-00','amandojsanatana@outlook.com','M','21/02/1961')
 	,('Sheila Carvalho Leal','456.789.098-23','scarvalho@ig.com.br','F','13/09/1978')
 	,('Carlos Henrique Souza','765.982.782-99','chenrique@ig.com.br','M','08/09/1981')
 	,('Maria Aparecida Souza','873.653.098-99','mapdasouza@outlook.com','F','07/07/1962')
@@ -17,19 +17,25 @@ VALUES
 
 INSERT INTO tbFornecedor (nomeFornecedor,contatoFornecedor)
 VALUES
-	('Atacad„o','Carlos Santos')
+	('Atacad√£o','Carlos Santos')
 	,('Assai','Maria Stella')
-	,('Rold„o','Paulo CÈsar')
+	,('Rold√£o','Paulo C√©sar')
 
 INSERT INTO tbProduto (descricaoProduto,valorProduto,quantidadeProduto)
 VALUES
 	('Amaciante Downy',5.76,1500)
 	,('Amaciante Comfort',5.45.2300)
-	,('Sab„o em pÛ OMO',5.99,1280)
+	,('Sab√£o em p√≥ OMO',5.99,1280)
 	,('Margarina Qually',4,76,2500)
 	,('Salsicha Hot Dog Saddia',6.78,2900)
-	,('Mortadela Perdig„o',2.50,1200)
+	,('Mortadela Perdig√£o',2.50,1200)
 	,('Hamburguer Saddia',9.89,1600)
 	,('Fralda Pampers',36,340)
 	,('Xampu Seda',5.89,800)
 	,('Condicionador Seda',6.50,700)
+
+INSERT INTO tbVenda
+tbVenda(dataVenda,valorTotalVenda)
+VALUES
+(01/02/2014,4500)
+,(03/02/2014,3400)
