e9356ed382b80c5792a4effc522f452464caf71b
EduardoCherubim <142914224+EduardoCherubim@users.noreply.github.com>
2024-03-14 08:17:20 -0300

    Atualizar o SqlQuery3.sql

--- SqlQuery3.sql
+++ SqlQuery3.sql
@@ -1,3 +1,21 @@
 USE bdEstoque
 
-UPDATE INTO bdProfuto
+UPDATE INTO bdProduto (valorProduto)
+VALUES(5.99)
+,(6.58)
+,(6.47)
+,(7.15)
+WHERE codFabricante = 1
+
+UPDATE INTO bdProduto (valorProduto)
+VALUES (5.35)
+,(33.48)
+WHERE codFabricante = 2
+
+UPDATE INTO bdProduto (quantidadeProduto)
+VALUES (1470)
+,(2270)
+,(2470)
+,(1570)
+WHERE codFornecedor = 1
+
